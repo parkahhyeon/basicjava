908fa2402f9502dd2cb06464eaffeea7bb3d2f39
ahhyeon <from.ahpark@gmail.com>
2023-04-15 03:39:14 +0900

    [Java] 제어자 : 기타 제어자 (static, final, abstract)와 제어자의 조합.

--- src/main/java/com/example/basicjava/BasicjavaApplication.java
+++ src/main/java/com/example/basicjava/BasicjavaApplication.java
@@ -763,9 +763,28 @@ public class BasicjavaApplication {
         String sameVar = "같은 패키지는 접근 허용"; // default 필드
     }
 
+    final class Bike {                  // 이 클래스는 상속을 통해 서브 클래스 생성 불가.
+        final int VAR = 0;                  // 이 필드는 상수화되어 값 변경 불가.
+        final void brake() {            // 이 메서드는 오버라이딩을 통해 재정의 불가.
+            final double MAX_NUM = 10.2; // 이 지역 변수는 상수화되어 값 변경 불가.
+        }
+    }
 
+    class Car {
+        static int var;     // 클래스 필드 (static 변수)
+        static {            // static 초기화 블록
+            // 보통 클래스 필드의 초기화를 진행함.
+            {
+                static void brake() {   // 클래스 메서드 (static 메서드)
 
+            }
+            }
+        }
+    }
 
+    abstract class Car {    // 추상 클래스
+        abstract void brake();  // 추상 메서드
+    }
 
 
 }
